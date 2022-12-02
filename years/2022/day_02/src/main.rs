1457741d6f6a1bb84a5f75e6e715d250e0422289
pandorasNox <itspandorasnox@gmail.com>
2022-12-02 21:25:50 +0100

    2022_day-02 - add solution puzzle 2

--- years/2022/day_02/src/main.rs
+++ years/2022/day_02/src/main.rs
@@ -20,7 +20,8 @@ fn main() {
       .expect("Should have been able to read the file");
     // println!("input content:\n{}", input_content);
 
-    println!("total_score_from_strategy_guide: {}", total_score_from_strategy_guide(input_content))
+    println!("total_score_from_strategy_guide: {}", total_score_from_strategy_guide(input_content.clone()));
+    println!("total_score_from_outcome_strategy_guide: {}", total_score_from_outcome_strategy_guide(input_content));
 }
 
 fn total_score_from_strategy_guide(input: String) -> u32 {
@@ -142,6 +143,136 @@ impl HandShapes {
 fn decryp(encryp: String) -> HandShapes {
     return HandShapes::Rock;
 }
+
+
+// ----------------------------------------------------------------------------
+
+#[derive(Debug)]
+enum Should {
+    Win,
+    Lose,
+    Draw,
+}
+
+#[derive(Debug)]
+struct OutcomeStrategy(HandShapes, Should);
+
+fn total_score_from_outcome_strategy_guide(input: String) -> u32 {
+    let outcome_strategy_guide = parse_to_outcome_strategy_guide(input).unwrap();
+    
+    let mut points: u32 = 0;
+    for move_strategy in outcome_strategy_guide {
+        println!("DEBUG total_score_from_strategy_guide game_move:'{:?}'", move_strategy);
+
+        let OutcomeStrategy(oponent, should_outcome) = move_strategy;
+
+        match (oponent, should_outcome) {
+            (HandShapes::Rock, Should::Win) => {
+                //choose paper
+                points += 2;
+                //should win
+                points += 6;
+            }
+            (HandShapes::Rock, Should::Lose) => {
+                //choose Scissors
+                points += 3;
+                //should win
+                points += 0;
+            }
+            (HandShapes::Rock, Should::Draw) => {
+                //choose Rock
+                points += 1;
+                //should win
+                points += 3;
+            }
+
+            (HandShapes::Paper, Should::Win) => {
+                //choose Scissors
+                points += 3;
+                //should win
+                points += 6;
+            }
+            (HandShapes::Paper, Should::Lose) => {
+                //choose Rock
+                points += 1;
+                //should win
+                points += 0;
+            }
+            (HandShapes::Paper, Should::Draw) => {
+                //choose paper
+                points += 2;
+                //should win
+                points += 3;
+            }
+            (HandShapes::Scissors, Should::Win) => {
+                //choose rock
+                points += 1;
+                //should win
+                points += 6;
+            }
+            (HandShapes::Scissors, Should::Lose) => {
+                //choose paper
+                points += 2;
+                //should win
+                points += 0;
+            }
+            (HandShapes::Scissors, Should::Draw) => {
+                //choose Scissors
+                points += 3;
+                //should win
+                points += 3;
+            }
+        }
+    }
+
+    return points;
+}
+
+// fn score_value(&self) -> u8 {
+//     match self {
+//         HandShapes::Rock => {return 1;},
+//         HandShapes::Paper => {return 2},
+//         HandShapes::Scissors => {return 3},
+//     }
+// }
+
+fn parse_to_outcome_strategy_guide(input: String) -> Option<Vec<OutcomeStrategy>> {
+    let input_lines = input.split("\n");
+
+    let mut strategy_guide = Vec::<OutcomeStrategy>::new();
+    for (line_nr, line) in input_lines.enumerate() {
+        println!("DEBUG parse_to_strategy_guide: line_nr='{}' content='{}'", line_nr, line);
+
+        let line_moves = line.split(" ")
+            .collect::<Vec<&str>>()
+            .iter()
+            .map(|m| m.chars().next().unwrap())
+            .collect::<Vec<char>>();
+
+        if line_moves.len() != 2 {
+            return None;
+        }
+
+        let opnents_move = map_to_handshape(*line_moves.first().unwrap()).unwrap();
+        let should_play_move = map_to_should_outcome(*line_moves.last().unwrap()).unwrap();
+
+        strategy_guide.push(OutcomeStrategy(opnents_move, should_play_move));
+    }
+
+    return Some(strategy_guide);
+}
+
+fn map_to_should_outcome(c: char) -> Option<Should> {
+    match c {
+        'X' => return Some(Should::Lose),
+        'Y' => return Some(Should::Draw),
+        'Z' => return Some(Should::Win),
+        _ => None
+    }
+}
+
+// ----------------------------------------------------------------------------
+
 
 #[cfg(test)]
 mod tests {
